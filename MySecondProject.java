16805c96190204f5d032f53dd41838c6de8e3616
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2023-08-02 16:18:39 -0300

    first

---
+++ MySecondProject.java
@@ -0,0 +1,14 @@
+package mysecondproject;
+
+public class MySecondProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello again world");
+
+    }
+    
+}

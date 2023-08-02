a5007b80ba67cb194875dac9121d8c79873d3051
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2023-08-02 16:21:07 -0300

    02

---
+++ Eclipse/MySecondProject/src/mysecondproject/HelloMain.java
@@ -0,0 +1,10 @@
+package mysecondproject;
+
+public class HelloMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello again world");
+	}
+
+}

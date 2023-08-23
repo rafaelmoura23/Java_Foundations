f571fc36a1cc179658bfd8333898bb709c905597
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2023-08-23 16:52:32 -0300

    class

---
+++ NetBeans/Class and Method/src/pkgclass/and/method/ClassAndMethod.java
@@ -0,0 +1,25 @@
+package pkgclass.and.method;
+
+public class ClassAndMethod {
+
+    public static void main(String[] args) {
+        
+        double p1 = 10;
+        double p2 = 12;
+        double p3 = 9;
+        double p4 = 8;
+        double p5 = 7;
+        double p6 = 15;
+        double p7 = 11;
+        double p8 = 30;
+        
+        p1 = (p1*0.05) + (p1*0.15)+ p1;
+        p1 = (p1*0.05) + (p1*0.15)+ p1;
+        
+        System.out.println(p1);
+            
+        
+        
+    }
+    
+}
